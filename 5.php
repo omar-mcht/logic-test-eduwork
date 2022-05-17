e1b3632153882da25f463b3920fc029b5bba5a7b
unknown <omar.mochtr@gmail.com>
2022-05-17 17:20:45 +0700

    revisi no 5

--- 5.php
+++ 5.php
@@ -1,24 +1,42 @@
 <?php
 
-$konversi = array("Puluh", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "100");
 
-$nilai = 24;
-$panjang = strlen($nilai);
-echo "Input Angka = ". $nilai ."<br><br><hr><br>";
-echo "Output Huruf = ";
-if ($nilai == 100) {
-    echo "Seratus";
-}elseif ($nilai == 0) {
-    echo "Silahkan masukkan bilangan antara 1-100";
-}elseif ($panjang == 1) {
-    echo $konversi[$nilai];
-}elseif ($panjang == 2) {
-    $a = substr($nilai, 0, 1);
-    $b = substr($nilai, 1, 1);
-    echo $konversi[$a] . " ";
-    echo $konversi[$b];
-}else {
-    echo "Silahkan masukkan bilangan antara 1-100";
+konversi_angka(100);
+
+function konversi_angka($angka){
+    $konversi = array("Puluh", "Satu", "Dua", "Tiga", "Empat", "Lima", "Enam", "Tujuh", "Delapan", "Sembilan", "Sepuluh", "100");
+    $panjang = strlen($angka);
+    
+    echo "Input Angka = ". $angka ."<br><br><hr><br>";
+    echo "Output Huruf = ";
+    if ($angka == 100) {
+        echo "Seratus";
+    }elseif ($angka == 0) {
+        echo "Silahkan masukkan bilangan antara 1-100";
+    }elseif ($angka == 10) {
+        echo "Sepuluh";
+    }elseif ($angka == 11) {
+        echo "Sebelas";
+    }
+    elseif ($angka > 11 and $angka < 20) {
+        $a = substr($angka, 1, 1);
+        echo $konversi[$a]. " Belas"; 
+    }elseif ($panjang == 2 AND substr($angka, 1, 1) > 0) {
+        $a = substr($angka, 0, 1);
+        $b = substr($angka, 1, 1);
+        echo $konversi[$a] . " Puluh ";
+        echo $konversi[$b];
+    }elseif ($panjang == 2) {
+        $a = substr($angka, 0, 1);
+        $b = substr($angka, 1, 1);
+        echo $konversi[$a] . " ";
+        echo $konversi[$b];
+    }else {
+        echo "Silahkan masukkan bilangan antara 1-100";
+    }
+
 }
+
+
 
 ?>
